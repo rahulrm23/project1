044ca6e6056b6bbf5a9b97e50209bc47253e2d68
rahulrm23 <rahulrajarbm@gmail.com>
2024-09-03 12:11:08 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,46 @@
-module
+provider "aws" {
+  region = "us-west-2"  # Change this to your preferred region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block  = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Change to your preferred AZ
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
