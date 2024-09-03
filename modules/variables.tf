01a920e2da0d3678182b20a4901a8e3688663190
rahulrm23 <rahulrajarbm@gmail.com>
2024-09-03 12:11:26 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,1 +1,17 @@
 
+# variables.tf
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  default     = "10.0.1.0/24"
+}
