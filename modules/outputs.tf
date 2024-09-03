b30c3c048c276f30b3e419bf562156b0503e0a87
rahulrm23 <rahulrajarbm@gmail.com>
2024-09-03 12:11:59 +0530

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,1 +1,14 @@
+# outputs.tf
+
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "internet_gateway_id" {
+  value = aws_internet_gateway.igw.id
+}
 
